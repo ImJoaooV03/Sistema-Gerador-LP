c2cc5346902b23b5ee69d0d5b2e4a677672aab62
ImJoaooV03 <joserengeldesign@gmail.com>
2026-04-04 10:48:04 -0300

    feat: enable realtime on dashboard tables

---
+++ supabase/migrations/20260404100000_enable_realtime.sql
@@ -0,0 +1,5 @@
+ALTER PUBLICATION supabase_realtime ADD TABLE projetos;
+ALTER PUBLICATION supabase_realtime ADD TABLE referencias;
+ALTER PUBLICATION supabase_realtime ADD TABLE design_systems;
+ALTER PUBLICATION supabase_realtime ADD TABLE paginas_geradas;
+ALTER PUBLICATION supabase_realtime ADD TABLE jobs;
